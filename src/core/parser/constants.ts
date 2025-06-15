f8be195c3a790821b48b406e79f676a806ea13d7
sam hoang <samhv.ict@gmail.com>
2025-06-15 07:28:03 +0700

    fix Inefficient regular expression

--- src/core/parser/constants.ts
+++ src/core/parser/constants.ts
@@ -1,4 +1,4 @@
-export const STATIC_OPENING_TAG_REGEX = /<\s*([\w:-]+)((?:\s+[\w:-]+(?:=(?:"[^"]*"|'[^']*'|[^\s/>]+))?)*\s*)?(\/?)\>/
+export const STATIC_OPENING_TAG_REGEX = /<\s*([\w:-]+)([^>]*?)(\/?)\>/
 export const STATIC_CLOSING_TAG_REGEX = /<\/\s*([\w:-]+)\s*>/
 
 export const COMMON_ENTITIES: Record<string, string> = {
